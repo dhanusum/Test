41d1f489c60c5c5cd7eff5d6bf52fb0c55f761a0
vinay <dhanusum@proteantech.in>
2023-08-08 21:26:01 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [formData, setFormData] = useState({ data: "" });
 
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const SubmitAction = async (event) => {
+                event.preventDefault();
+            
+                try {
+                  const response = await axios.post('https://api.example.com/post-endpoint', formData);
+            
+                  console.log('Response from server:', response.data);
+                  // Handle the response data as needed
+                } catch (error) {
+                  console.error('Error:', error);
+                  // Handle errors
+                }
+              };
+    return (
+        <div>
+            <h1><center>Login Form</center></h1>
+            <label>Enter UserID : </label>
+            <input name='username' style={{ fontFamily: "Bold" }}
+                className="form-control form-control-lg" placeholder='UserID'
+                onChange={(e) => handleChange(e)} required={true} />
+            <label>Enter Password : </label>
+            <input name="password" style={{ fontFamily: "Bold" }}
+                className="form-control form-control-lg" type="password"
+                placeholder='Password' onChange={(e) => handleChange(e)} required={true} />
+            <button onClick={SubmitAction}>Submit</button>
+        </div>
+    )
+
+}
 export default App;
